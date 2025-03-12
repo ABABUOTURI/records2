6117dd941e100e2ecc7baad177bd3fec696dfeb3
Legend <ababuoturi@gmail.com>
2025-03-12 08:35:47 +0300

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client"; // Ensure it's a Client Component
 
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
